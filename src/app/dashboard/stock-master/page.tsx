8b122278f10c847540106ff4dc2c3b5741aa955a
Kaustubh Ashok Chavan <kaustubhashokchavan@Kaustubhs-MacBook-Pro.local>
2025-09-29 11:00:25 +0530

    third commit

--- src/app/dashboard/stock-master/page.tsx
+++ src/app/dashboard/stock-master/page.tsx
@@ -1,4 +1,4 @@
-
+'use client';
 import Link from 'next/link';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { PackagePlus, LayoutGrid, Truck, Box } from 'lucide-react';
